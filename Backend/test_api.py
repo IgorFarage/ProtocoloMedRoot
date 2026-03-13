4e0556317bfca816d8afbbb103542246b5cb1029
Igor <igor.farage@gmail.com>
2026-03-13 12:00:57 -0300

    chore: Update financial migrations and prod env for Asaas integration

--- Backend/test_api.py
+++ Backend/test_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-r1 = requests.post("http://localhost:8000/api/accounts/token/", json={"email": "plano54@teste.com", "password": "senhadoteste"})
+r1 = requests.post("http://localhost:8000/api/accounts/token/", json={"email": "plano54@teste.com", "password": "***"})
 token = r1.json().get('access')
 
 r2 = requests.get("http://localhost:8000/api/medical/appointments/", headers={"Authorization": f"Bearer {token}"})
